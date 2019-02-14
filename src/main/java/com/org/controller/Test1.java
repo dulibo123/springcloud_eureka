9bc206f1157a860c985b95340010c58320cb7a6d
haha <1737257420@qq.com>
2019-02-14 15:23:14 +0800

    eureka的注册中心加类

--- src/main/java/com/org/controller/Test1.java
+++ src/main/java/com/org/controller/Test1.java
@@ -2,6 +2,6 @@ package com.org.controller;
 
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("i am zhangsan");
+        System.out.println("i am zhangsan123");
     }
 }
